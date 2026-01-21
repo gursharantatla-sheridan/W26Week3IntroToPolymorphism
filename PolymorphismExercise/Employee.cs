92d858276a10c9c36d264de251cfa847819c8e1a
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-21 14:35:29 -0500

    emp class

---
+++ PolymorphismExercise/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismExercise
+{
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public Employee(int id, string? name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public abstract double Earnings();
+
+        public override string ToString()
+        {
+            return $"Id = {Id}\nName = {Name}";
+        }
+    }
+}
