14aad9e20befb122cf784ae661c3fa5350dca676
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-21 14:50:10 -0500

    hourly emp class

---
+++ PolymorphismExercise/HourlyEmployee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismExercise
+{
+    public class HourlyEmployee : Employee
+    {
+        public int Hours { get; set; }
+        public double Wage { get; set; }
+
+        public HourlyEmployee(int id, string? name, int hours, double wage) : base(id, name)
+        {
+            Hours = hours;
+            Wage = wage;
+        }
+
+        public override double Earnings()
+        {
+            if (Hours <= 40)
+                return Hours * Wage;
+            else
+                return (40 * Wage) + (Hours - 40) * Wage * 1.5;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nHours = {Hours}\nWage = {Wage:C}";
+        }
+    }
+}
