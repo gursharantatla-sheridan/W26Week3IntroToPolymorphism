f18b4063bc7f4952bebacd47b22a77b67d8f6859
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-21 14:41:40 -0500

    salaried emp

---
+++ PolymorphismExercise/SalariedEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismExercise
+{
+    public class SalariedEmployee : Employee
+    {
+        public double Salary { get; set; }
+
+        public SalariedEmployee(int id, string name, double salary) : base(id, name)
+        {
+            Salary = salary;
+        }
+
+        public override double Earnings()
+        {
+            return Salary;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nSalary = {Salary:C}";
+        }
+    }
+}
