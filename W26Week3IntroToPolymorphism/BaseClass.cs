aef97677946add677d9a2ac5d947fe14bf1bf6e4
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-21 13:56:47 -0500

    dynamic polym

---
+++ W26Week3IntroToPolymorphism/BaseClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace W26Week3IntroToPolymorphism
+{
+    public class BaseClass
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("Base class Show method");
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Derived class Show method");
+        }
+    }
+}
--- W26Week3IntroToPolymorphism/Program.cs
+++ W26Week3IntroToPolymorphism/Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int x = 5;
+            //Add(2, 3, ref x);
+
+            //Add(2, 3);
+
+
+            BaseClass objBase;
+            objBase = new BaseClass();
+            objBase.Show();
+
+            objBase = new DerivedClass();
+            objBase.Show();
+            
+            DerivedClass objDerived = new DerivedClass();
+            objDerived.Show();
+        }
+
+        static void PrintArray(int[] array)
+        {
+
+        }
+
+        //static void PrintArray(params int[] array)
+        //{
+
+        //}
+
+        static void Add(int n1, int n2)
+        {
+
+        }
+
+        //static int Add(int n1, int n2)
+        //{
+
+        //}
+
+        static void Add(string n1, string n2)
+        {
+
+        }
+
+        static void Add(int n1, int n2, int n3)
+        {
+
+        }
+
+        static void Add(int n1, int n2, ref int n3)
+        {
+
         }
     }
 }
